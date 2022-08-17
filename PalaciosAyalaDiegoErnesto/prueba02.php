f665207168b2f790dac22a782cbb00cb80dad654
Parker092 <53531818+Parker092@users.noreply.github.com>
2022-08-16 18:44:21 -0600

    Guia 1 - PAginas Web Con Software Libre

---
+++ PalaciosAyalaDiegoErnesto/prueba02.php
@@ -0,0 +1,17 @@
+<?php include "includes/header.php"; ?>
+
+<?php
+echo "hola mundo";
+echo"</br>";
+echo ("hola mundo");
+echo"</br>";
+print_r("hola mundo");
+echo"</br>";
+var_dump("hola mundo");
+echo"</br>";
+$nombre="Pepe";
+echo "Hola <b> $nombre </b>, bienvenido a clase";
+echo "hola".$nombre."gusto de verte"
+?>
+
+<?php include "includes/footer.php"; ?>
